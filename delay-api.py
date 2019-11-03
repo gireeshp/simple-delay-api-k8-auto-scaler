553804aa98bdd496d8de0dbbd80ee1a1cc1b6a5a
Gireesh Puthumana <gireesh@Gireeshs-MacBook-Pro.local>
2019-11-03 23:14:11 +0530

    added simple wait instead of CPU intensive delay() method

--- delay-api.py
+++ delay-api.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from time import sleep
 from delay import Delay
 
 app = Flask(__name__)
 
-@app.route("/delay")
+@app.route("/delay", methods=['GET', 'POST'])
 def delay():
-	print ("Inside")
-	d = Delay()
-	d.start_a_delay(4, 200, lambda : (False))
-	return "Successful", 200
+	# d = Delay()
+	# d.start_a_delay(4, 200, lambda : (False))
+
+	sleep(10)
+
+	resp = "Successful"
+
+	if request.method == 'POST':
+		input_json = request.get_json()
+		resp = jsonify({"Input received": input_json})
+
+	return resp, 201
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8081)
